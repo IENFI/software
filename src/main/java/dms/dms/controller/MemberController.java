b657332d06ce4d67121142c7b8cce3420d903578
IENFI <tunnel0.rabbit@gmail.com>
2023-11-08 11:52:05 +0900

    231108 dms_login init

---
+++ src/main/java/dms/dms/controller/MemberController.java
@@ -0,0 +1,48 @@
+package dms.dms.controller;
+
+import dms.dms.dto.MemberDTO;
+import dms.dms.service.MemberService;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequiredArgsConstructor // MemberService에 대한 멤버를 사용 가능
+public class MemberController {
+
+    // 생성자 주입
+    private final MemberService memberService;
+
+    // 회원가입 페이지 출력 요청
+    @GetMapping("/member/save")
+    public String saveForm() {
+        return "save";
+    }
+
+    @PostMapping("/member/save")    // name값을 requestparam에 담아온다
+    public String save(@ModelAttribute MemberDTO memberDTO) {
+        System.out.println("MemberController.save");
+        System.out.println("memberDTO = " + memberDTO);
+        memberService.save(memberDTO);
+
+        return "index";
+    }
+
+
+    @PostMapping("/member/login")
+    public String login(@ModelAttribute MemberDTO memberDTO, HttpSession session){
+        MemberDTO loginResult = memberService.login(memberDTO);
+        if (loginResult != null) {
+            // login 성공
+            session.setAttribute("loginEmail", loginResult.getMemberEmail());
+            return "main";
+        } else {
+            // login 실패
+            return "index";
+        }
+    }
+}
